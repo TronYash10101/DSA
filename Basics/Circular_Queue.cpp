e7a4e30fcdc66f03cec0576b0a4cd6b6553c13d5
Yash Jadhav <8f34yashjadhav@gmail.com>
2024-03-13 00:09:32 +0530

    c-2

--- Basics/Circular_Queue.cpp
+++ Basics/Circular_Queue.cpp
@@ -1,8 +1,34 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
+class queue
+{
+public:
+    int *arr;
+    int data;
+    int front = 0;
+    int back = 0;
+
+    queue(int max_size)
+    {
+
+        this->arr = new int[max_size];
+    }
+
+    void enque(int size)
+    {
+        if ((front + 1) % size == back)
+        {
+            cout << "Queue Full";
+        }
+        front += 1;
+    }
+};
+
 int main()
 {
+    queue obj(5);
+
     return 0;
 }
